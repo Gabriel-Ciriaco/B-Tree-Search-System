dd176695038199731a63ffb340e62f05ccb4932f
Carol-Nunes <cacanunesaraujo@gmail.com>
2025-09-16 12:13:52 -0300

    Criação do Sistema
    Criação de um sistema interativo que executa inserção, remoção e busca conforme escolha do usuário.

---
+++ system/system.h
@@ -0,0 +1,19 @@
+#ifndef SYSTEM_H_INCLUDED
+#define SYSTEM_H_INCLUDED
+
+#include "bTree.h"
+
+typedef struct
+{
+
+    BT *arvore;
+
+}Sistema;
+
+void mensagemBoasVindas();
+
+int rodarMenu();
+
+void executarSistema();
+
+#endif // SYSTEM_H_INCLUDED
